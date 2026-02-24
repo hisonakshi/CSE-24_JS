d0fe7ae688cb3b0e9d8e72a534a3887c30c476c7
hisonakshi <@hisonakshi>
2026-02-24 16:29:24 +0530

    operation Assignment

---
+++ assignment.js
@@ -0,0 +1,33 @@
+let a=45;
+let b=25;
+let linebreak = "</br>";
+
+document.write("a+b =");
+result = a+b;
+document.write(result);
+document.write(linebreak);
+
+
+document.write("a-b =");
+result = a-b;
+document.write(result);
+document.write(linebreak);
+
+
+document.write("a*b =");
+result = a*b;
+document.write(result);
+document.write(linebreak);
+
+
+document.write("a/b =");
+result = a+b;
+document.write(result);
+document.write(linebreak);
+
+
+document.write("a%b =");
+result = a%b;
+document.write(result);
+document.write(linebreak);
+
