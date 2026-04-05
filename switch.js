ea5d3b3909a376d22724e8691477fc5c614f7bf8
Sonakshi <hisonakshihere@gmail.com>
2026-04-05 12:34:46 +0530

    Updated switch statements

---
+++ switch.js
@@ -0,0 +1,47 @@
+var grade = "B";
+
+switch (grade) {
+    case "A":
+        console.log("Excellent");
+        break;
+    case "B":
+        console.log("Good");
+        break;
+    case "C":
+        console.log("Average");
+        break;
+    case "D":
+        console.log("Poor");
+        break;
+    default:
+        console.log("Invalid grade");
+}
+
+
+var dayNumber = 4; // 1 = Monday, 2 = Tuesday, ...
+
+switch (dayNumber) {
+    case 1:
+        console.log("Monday");
+        break;
+    case 2:
+        console.log("Tuesday");
+        break;
+    case 3:
+        console.log("Wednesday");
+        break;
+    case 4:
+        console.log("Thursday");
+        break;
+    case 5:
+        console.log("Friday");
+        break;
+    case 6:
+        console.log("Saturday");
+        break;
+    case 7:
+        console.log("Sunday");
+        break;
+    default:
+        console.log("Invalid day");
+}
