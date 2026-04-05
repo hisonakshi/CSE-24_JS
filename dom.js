0466d72f890d09f6272141f7c25fce5bf6119aac
Sonakshi <hisonakshihere@gmail.com>
2026-04-05 12:41:37 +0530

    Add JS and HTML files

---
+++ dom.js
@@ -0,0 +1,52 @@
+// DOCUMENT OBJECT MODel , REACT [SPA-single page application]
+//JS can change all the html page elements in the page 
+//JS can change all the attributes of page
+//JS can change the css styles of page
+//JS can remove th existing elements and attributes 
+//JS can create new HTML events in the page.
+
+// let x = document.getElementById("demo");
+// x.innerHTML = "HELLO WORLD";
+
+// let z = document.getElementsByTagName("h3")
+// y.innerHTML = "I am Tag name";
+
+// let y = document.getElementsByClassName("hello");
+// y.innerHTML = "I am class name";
+
+
+// document.querySelector(#id,.class);
+// document.querySelectorAll;
+
+
+// let x = document.getElementById("demo").getAttribute("id");         // demo
+// console.log(x)
+
+
+// let d = document.getElementById("demo").setAttribute("class","demoClass"); //attribute name , attribute 
+// console.log(d)
+
+
+// let x = document.getElementById("demo").style.Color = "blue";
+// x.innerHTML = "hello world";
+// console.log(x);
+
+
+let c = document.createElement("h2");
+let parent = document.getElementsByTagName("body");
+parent[0].append(c);            //FOR ADDING 
+
+c.remove();             //FOR REMOVAL
+
+
+
+
+
+
+
+
+
+
+
+
+
